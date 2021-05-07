95c84611fa8002b7db7f2ac078e800f5281c69e4
Brandon G.M <brandongmerritt@gmail.com>
2021-05-06 21:05:07 -0400

    Theres puppies on our pages

--- controllers/index.js
+++ controllers/index.js
@@ -1,31 +1,29 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const path = require('path');
-const DogBreed = ('https://dog.ceo/api/breeds/list/all');
+const DogBreed = ('https://api.thedogapi.com/v1/images/search');
 const { get } = require('./api');
 const fetch = require("node-fetch");
 router.use('/api', apiRoutes);
-
 router.get("/", (req, res) => {
     console.log("We in dis!!");
     //res.sendFile(path.join(__dirname, "../public/index.html"));
     res.render('homepage', {layout: 'main'});
 });
 
+router.get('/api/randomdog', (req,res) =>{
+    fetch(DogBreed)
+.then(response => response.json())
+.then(data => res.json(data));
+})
 // router.get("/account", (req, res) =>{
 //     res.render('account');
 // });
 
-fetch(DogBreed)
-.then(response => response.json())
-.then(data => console.log(data));
+
 
 router.get("/login", (req, res) =>{
     res.render('login');
 });
 
-
 module.exports = router;
-
-
-fetch('http://example.com/movies.json')
--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,50 @@
-var petfinder = require("@petfinder/petfinder-js");
-var client = new petfinder.Client({apiKey: "my-api-key", secret: "my-api-secret"});
+// var petfinder = require("@petfinder/petfinder-js");
+// var client = new petfinder.Client({apiKey: "my-api-key", secret: "my-api-secret"});
+// const DogBreed = ('https://api.thedogapi.com/v1/images/search');
+// const fetch = require("node-fetch");
+// client.animal.search()
+//     .then(function (response) {
+//         // Do something with `response.data.animals`
+//     })
+//     .catch(function (error) {
+//         // Handle the error
+//     });
+// const HardPass = document.getElementById('NoDeal');
+// const Absolutely = document.getElementById('Absolutely')
+// HardPass.addEventListener('click', function(){
+//     alert('helloWorld');
+// });
 
-client.animal.search()
-    .then(function (response) {
-        // Do something with `response.data.animals`
+// Absolutely.addEventListener('click', function(){
+//     alert('helloWorld');
+// })
+
+// fetch('api/randomdog')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+// const AnimalDisplay = document.getElementById('petdisplay')
+// AnimalDisplay.setAttribute(DogBreed)
+
+const imageRandom = document.getElementById("imageRandom");
+
+function getRandomImage(){
+  const randomImageApiUrl = "api/randomdog";
+  fetch(randomImageApiUrl)
+    .then(function(response){
+      return response.json();
     })
-    .catch(function (error) {
-        // Handle the error
+    .then(function(json){
+      // now we got the json . we can use this to update any data in html 
+    //   console.log(json);
+      var imageUrl = json[0].url;
+      console.log(imageUrl)
+      //update the image with new random url
+      imageRandom.src=imageUrl;
+    })
+    .catch(function(error){
+      console.log(error);
     });
+}
+//call the getRandomImage function whenever page loads
+getRandomImage();
