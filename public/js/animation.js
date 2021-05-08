a7bf4bc05e933a89c014d8d823ad6c1098868702
Brandon G.M <brandongmerritt@gmail.com>
2021-05-08 02:08:42 -0400

    animation station

---
+++ public/js/animation.js
@@ -0,0 +1,9 @@
+gsap.fromTo(".IwolfU",
+    { opacity: 0, scale: 0 },
+    { opacity: 1, scale: 1, duration: 1.15 }
+  );
+
+  gsap.fromTo(".card-loader",
+  { opacity: 0, scale: 0 },
+  { opacity: 1, scale: 1, duration: 1.15 }
+);
