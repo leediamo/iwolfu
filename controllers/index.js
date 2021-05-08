55cc43a30b0614a024e96ce1d686bbf4589b5f0b
Kyle Nance <nancekyle15@gmail.com>
2021-05-08 01:02:09 -0400

    login page

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+//const apiRoutes = require('./api');
 const path = require('path');
 const DogBreed = ('https://dog.ceo/api/breeds/list/all');
-const { get } = require('./api');
+//const { get } = require('./controllers/api');
 const fetch = require("node-fetch");
-router.use('/api', apiRoutes);
+const UserRoutes = require('./api/userRoutes');
+
+
+
+router.use('/api', UserRoutes);
 
 router.get("/", (req, res) => {
     console.log("We in dis!!");
-    //res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
     res.render('homepage', {layout: 'main'});
 });
 
@@ -16,16 +20,11 @@ router.get("/", (req, res) => {
 //     res.render('account');
 // });
 
-fetch(DogBreed)
-.then(response => response.json())
-.then(data => console.log(data));
-
 router.get("/login", (req, res) =>{
     res.render('login');
 });
 
-
 module.exports = router;
 
 
-fetch('http://example.com/movies.json')
+fetch('')
